3ba29b8ea3a687038a34be47eeaefa03fa30fd64
Tanzeel <ktanzeel80@gmail.com>
2022-07-23 19:52:37 +0530

    Editing navbar

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<div class="navbar navbar-inverse navbar-top" style="background-color: rgba(0, 0, 0, 0.7); display: flex;">
+<!-- <div class="navbar navbar-inverse navbar-top" style="background-color: rgba(0, 0, 0, 0.7); display: flex;">
 	<div class="container">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -17,4 +17,33 @@
 			</ul>
 		</div>
 	</div>
+</div> -->
+
+<div class="topnav" id="myTopnav">
+	<a href="#home" class="active">Home</a>
+	<a href="#news">News</a>
+	<a href="#contact">Contact</a>
+	<div class="dropdown">
+		<button class="dropbtn">Dropdown
+			<i class="fa fa-caret-down"></i>
+		</button>
+		<div class="dropdown-content">
+			<a href="#">Link 1</a>
+			<a href="#">Link 2</a>
+			<a href="#">Link 3</a>
+		</div>
+	</div>
+	<a href="#about">About</a>
+	<a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
 </div>
+
+<script>
+	function myFunction() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+			x.className += " responsive";
+		} else {
+			x.className = "topnav";
+		}
+	}
+</script>
--- templates/index.php
+++ templates/index.php
@@ -7,6 +7,7 @@
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<meta name="viewport" content="width=device-width" initial-scale=1>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="../public/css/style.css">
 	<title>Mantoes</title>
 </head>
