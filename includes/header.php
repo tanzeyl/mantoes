7b2dd17f52803aa15863a3533d8047baf7c9d292
Tanzeel <ktanzeel80@gmail.com>
2022-07-23 19:56:33 +0530

    Editing navbar

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,3 @@
-<!-- <div class="navbar navbar-inverse navbar-top" style="background-color: rgba(0, 0, 0, 0.7); display: flex;">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="index.php" style="font-size: 30px; color: #edeb55; border: 3px solid black;">Mantoes</a>
-		</div>
-		<div class="collapse navbar-collapse" id="myNavbar">
-			<ul class="nav navbar-nav navbar-right">
-				<li style="font-size: 20px;"><a href="checkout.php" style="color: white;">Checkout</a></li>
-				<li style="font-size: 20px;"><a href="viewmaterials.php" style="color: white;">Available material</a></li>
-				<li style="font-size: 20px;"><a href="change_prices.php" style="color: white;">Prices</a></li>
-				<li style="font-size: 20px;"><a href="accounts.php" style="color: white;">Accounts</a></li>
-			</ul>
-		</div>
-	</div>
-</div> -->
-
 <div class="topnav" id="myTopnav">
 	<a href="#home" class="active">Home</a>
 	<a href="#news">News</a>
